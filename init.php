ce2611dbc69ad4c272f395eb0cff7a5e5ec0991c
raahimid <raahim.idrees@gmail.com>
2020-03-17 16:46:03 -0400

    changes

--- init.php
+++ init.php
@@ -1,16 +1,20 @@
 <?php
-    function OpenCon() {
-        $dbhost = "localhost";
-        $dbuser = "root";
-        $dbpass = "";
-        $db = "geek_text";
+/*Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'geek_text');
 
-        $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+/* Attempt to connect to MySQL database*/
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-        return $conn;
-    }
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-    function CloseCon($conn) {
-        $conn -> close();
-    }
+
+
+
 ?>
