5960a211df5fc02af763bd11a5889f363dcf5abb
raahimid <raahim.idrees@gmail.com>
2020-04-14 19:10:22 -0400

    updated book details

--- bookdetails.php
+++ bookdetails.php
@@ -1,7 +1,7 @@
 <?php
 // including database connection
 //require 'init.php';
-include 'config.php';
+include 'init.php';
 include 'header.php';
 
 $bookcode = $_GET['bookid'];
@@ -9,7 +9,7 @@ $bookcode = $_GET['bookid'];
 
  if(isset($_POST['hihi'])){
 
-  $result = mysqli_query($link, "INSERT INTO `cart` (`UserId`,`BookId`,`quantity`) VALUES ('$id',$bookcode,'1')");
+  $result = mysqli_query($conn, "INSERT INTO `cart` (`UserId`,`BookId`,`quantity`) VALUES ('$id',$bookcode,'1')");
 
   if($result->num_rows > 0){
 
@@ -25,11 +25,11 @@ $bookcode = $_GET['bookid'];
 }
 echo "<meta http-equiv='refresh' content='0'>"; /* REFRESH PAGE WHEN ADD TO CART GETS CLICKED */
 
-} 
+}
 
 
-$result = mysqli_query($link, "select comment,bookcover,BookTitle, PublisherName, BookRating, genre, Price,
-ReleaseDate, AuthorName from book left join review on book.bookid = review.bookid JOIN publisher on book.PublisherID = publisher.PublisherID
+$result = mysqli_query($conn, "select comment,bookcover,BookTitle, PublisherName, BookRating, genre, Price,
+ReleaseDate, AuthorName, book.AuthorID from book left join review on book.bookid = review.bookid JOIN publisher on book.PublisherID = publisher.PublisherID
 JOIN author on book.AuthorID = author.AuthorID
 where book.bookid=$bookcode");
 while($res = mysqli_fetch_array($result))
@@ -43,6 +43,7 @@ while($res = mysqli_fetch_array($result))
   $ReleaseDate = $res['ReleaseDate'];
   $BookRating = $res['BookRating'];
   $AuthorName = $res['AuthorName'];
+  $AuthorID = $res['AuthorID'];
 }
 
 
@@ -55,22 +56,22 @@ while($res = mysqli_fetch_array($result))
 <head>
   <meta charset="utf-8">
   <title>GeekText</title>
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
-  <!-- <link rel="stylesheet" href="css/bookdetails.css"> -->
-   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,900|Ubuntu&display=swap" rel="stylesheet"> 
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> 
-   
+  <conn rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   <conn rel="stylesheet" href="css/bookdetails.css">
+   <conn href="https://fonts.googleapis.com/css?family=Montserrat:400,900|Ubuntu&display=swap" rel="stylesheet">
+   <conn rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
    <!--rating css -->
-  <link rel="stylesheet" href="rating.css">
+  <conn rel="stylesheet" href="rating.css">
 </head>
 
 <body>
 
   <section id="title">
 
-    <div class="container-fluid">
+  <!-- <div class="container-fluid"> -->
 
       <!-- Nav Bar -->
       <!-- <nav class="navbar navbar-expand-lg navbar-dark">
@@ -101,24 +102,29 @@ while($res = mysqli_fetch_array($result))
 
 
 <!-- Title -->
-    <div class="row">
-      <div class="col-lg-6">
-        <h1><?php echo $BookTitle; ?></h1>
-        <img class="img-responsive img-thumbnail" src="images/<?php echo $cover; ?>">
-       </div>
-      <div class="col-lg-6">
-        <p class = "bookdetails" "author-name">Author: <?php echo $AuthorName; ?></p>
-        <p class = "bookdetails" "publisher-name">Publisher: <?php echo $PublisherName; ?></p>
-        <p class = "bookdetails" "book-genre">Genre: <?php echo $Genre; ?></p>
-        <p class = "bookdetails" "avg-rating">Avg. Rating: <?php echo $BookRating; ?></p>
-        <p class = "bookdetails" "publish-date">Published On: <?php echo $ReleaseDate; ?></p>
-        <p class = "bookdetails" "book-price">Price: <?php echo $Price; ?></p>
+<div class ="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <h2><?php echo $BookTitle; ?></h2>
+          <a class="lightbox" href="#bookimage">
+            <img class="img-responsive img-thumbnail" style="width:200px;height:300px display: block;
+            margin-left: 140px;
+            margin-right: auto;" src="images/<?php echo $cover; ?>">
+          </a>
+          <div class="lightbox-target" id="bookimage">
+            <img src="images/<?php echo $cover; ?>"/>
+          <a class="lightbox-close" href="#"></a>
+          </div>
+        <div class="col-lg-6" style="padding-top: 100px">
+          <a href="authordetails.php?AuthorID=<?php echo $AuthorID; ?>"><b>Author: </b><?php echo $AuthorName; ?></a>
+          <p class = "bookdetails" "publisher-name"><b>Publisher: </b><?php echo $PublisherName; ?></p>
+          <p class = "bookdetails" "book-genre"><b>Genre: </b><?php echo $Genre; ?></p>
+          <p class = "bookdetails" "avg-rating"><b>Avg. Rating: </b><?php echo $BookRating; ?></p>
+          <p class = "bookdetails" "publish-date"><b>Published On: </b><?php echo $ReleaseDate; ?></p>
+          <p class = "bookdetails" "book-price"><b>Price: </b><?php echo $Price; ?></p>
+        </div>
       </div>
-    </div>
 </div>
-
-
-    </div>
   </section>
 
 
@@ -138,11 +144,11 @@ while($res = mysqli_fetch_array($result))
 
 <section id="reviews">
 <!-- <div class="row">
- <h4>Reviews</h4> 
+ <h4>Reviews</h4>
 </div> -->
 
-<!--CSS FOR THE DISPLAYED STARS NEXT TO THE COMMENTS STARTS HERE - LEONXL - AL --> 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+<!--CSS FOR THE DISPLAYED STARS NEXT TO THE COMMENTS STARTS HERE - LEONXL - AL -->
+<conn rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 .checked {
   color: orange;
@@ -154,24 +160,12 @@ while($res = mysqli_fetch_array($result))
   <?php
 
   /* FETCH THE BOOKS - LEONXL - AL */
-  $result = mysqli_query($link, "SELECT Rating, Comment, bookcover, UserId, ReviewNickname FROM book
+  $result = mysqli_query($conn, "SELECT Rating, Comment, bookcover, UserId FROM book
   LEFT JOIN review ON book.bookid = review.bookid
   where book.bookid=$bookcode");
 
-
- /* QUERY TO GET NICKNAMES IF USER IS NOT SIGNED IN - LEONXL - AL */
- $resultForReviewNickname = mysqli_query($link, "SELECT `Rating`, `Comment`, `UserFirst`, `ReviewNickname`  FROM `review` 
- LEFT JOIN user ON review.UserID = user.UserID");
- 
-  while($res=mysqli_fetch_assoc($resultForReviewNickname)){
-      // $ReviewNickname = $res['ReviewNickname']; 
-  }    
-
- 
-
-
    /* QUERY FOR ALL USERNAMES IF USER IS NOT SIGNED IN - LEONXL - AL */
-   $resultForUserName = mysqli_query($link, "SELECT `Rating`, `Comment`, `UserFirst` FROM `user` 
+   $resultForUserName = mysqli_query($conn, "SELECT `Rating`, `Comment`, `UserFirst` FROM `user`
    LEFT JOIN review ON user.UserID = review.UserID");
    while($res=mysqli_fetch_assoc($resultForUserName)){
         $ForUserName=$res['UserFirst'];
@@ -179,22 +173,22 @@ while($res = mysqli_fetch_array($result))
 
 
 /*QUERY FOR BOOKS, USED TO VERIFY IF PERSON BOUGHT THE BOOK - LEONXL - AL */
-$resultPurchased = mysqli_query($link, "SELECT UserId, purchased.BookId, isPurchased, purchaseID FROM purchased 
+$resultPurchased = mysqli_query($conn, "SELECT UserId, purchased.BookId, isPurchased, purchaseID FROM purchased
 LEFT JOIN book ON purchased.bookid = book.bookId WHERE purchased.bookId = $bookcode;");
 
 /*FETCH IsPurchased Column of 1's - LEONXL - AL */
 while ($row = mysqli_fetch_assoc($resultPurchased)){
-$isPurchased = $row['isPurchased']; 
+$isPurchased = $row['isPurchased'];
 }
 
-$purchasedFlag = 1; /*FLAG USED TO COMPARE AGAINST isPurchased Column.  
+$purchasedFlag = 1; /*FLAG USED TO COMPARE AGAINST isPurchased Column.
                      If we assign variable to a 0 (no comment section will show) - LEONXL - AL */
 
 echo "<h4>Reviews</h4>";
 
 /*LOGIC: if customer bought the book, give option to comment - LEONXL - AL */
 echo "<div class='entire-commentsection'>";
-if($purchasedFlag == $isPurchased && $id){  
+if($purchasedFlag == $isPurchased && $id){
 
   echo "<form method ='POST'>";
   echo "<div class='rating'>";
@@ -213,9 +207,9 @@ if($purchasedFlag == $isPurchased && $id){
   /* DROPDOWN FOR POSTING ANONYMOUS OR NICKNAME */
   echo "<div class='postAs'>";
   echo "<select id='postAs' name='postAs' required>";
-  echo "<option value='$name'>$name</option>";
-  echo  "<option value='$nickname'>$nickname</option>";
+  echo "<option value='' disabled selected>Post as:</option>";
   echo  "<option value='Anonymous'>Anonymous</option>";
+  echo  "<option value='$name'> $name </option>";
   echo "</select>" . "<br>";
   echo "</div>";
 
@@ -223,69 +217,59 @@ if($purchasedFlag == $isPurchased && $id){
   echo   "<textarea id='Comment' name='Comment' rows='10' cols='60' required Placeholder='Leave a comment...'></textarea>" . "<br>";
   echo   "<button value='submit'>Submit</button>";
   echo "</form>";
-  
-} 
+
+}
 echo "</div>";
 
 
 
-$Rating = mysqli_real_escape_string($link, $_POST['Rating']); //SUPERGLOBAL - LEONXL - AL
-$Comment = mysqli_real_escape_string($link, $_POST['Comment']); //SUPERGLOBAL - LEONXL - AL 
+$Rating = mysqli_real_escape_string($conn, $_POST['Rating']); //SUPERGLOBAL - LEONXL - AL
+$Comment = mysqli_real_escape_string($conn, $_POST['Comment']); //SUPERGLOBAL - LEONXL - AL
 
 
-//INSERT COMMENT INTO DATABASE - LEONXL - AL 
+//INSERT COMMENT INTO DATABASE - LEONXL - AL
 if(isset($_POST['postAs']) && $_POST['postAs'] == 'Anonymous'){
-$sql = mysqli_query($link,"INSERT INTO `review` (`Comment`,`Rating`,`bookId`,`ReviewNickname`) VALUES ('$Comment','$Rating','$bookcode','None');");//USING BOOKCODE DECLARED AT TOP OF FILE - LEONXL - AL 
+$sql = mysqli_query($conn,"INSERT INTO `review` (`Comment`,`Rating`,`bookId`) VALUES ('$Comment','$Rating','$bookcode');");//USING BOOKCODE DECLARED AT TOP OF FILE - LEONXL - AL
 echo "<meta http-equiv='refresh' content='0'>"; //REFRESH WHEN SUBMIT COMMENT BUTTON IS CLICKED
 } elseif (isset($_POST['postAs']) && $_POST['postAs'] == $name){
-  $sql = mysqli_query($link,"INSERT INTO `review` (`Comment`,`Rating`,`bookId`,`UserID`,`ReviewNickname`) VALUES ('$Comment','$Rating','$bookcode','$id','None');");
+  $sql = mysqli_query($conn,"INSERT INTO `review` (`Comment`,`Rating`,`bookId`,`UserID`) VALUES ('$Comment','$Rating','$bookcode','$id');");
   echo "<meta http-equiv='refresh' content='0'>"; //REFRESH WHEN SUBMIT COMMENT BUTTON IS CLICKED
-}elseif(isset($_POST['postAs']) && $_POST['postAs'] == $nickname){
-  $sql = mysqli_query($link,"INSERT INTO `review` (`Comment`,`Rating`,`bookId`,`UserID`,`ReviewNickname`) VALUES ('$Comment','$Rating','$bookcode','$id','$nickname');");
-  echo "<meta http-equiv='refresh' content='0'>"; //REFRESH WHEN SUBMIT COMMENT BUTTON IS CLICKED
-} 
+}
 
 echo "<div class='displayed-rating'>";
-echo "<hr class='rating-border'>"; 
+echo "<hr class='rating-border'>";
 /* FETCHING COMMENTS AND STAR RATING AMOUNT TO DISPLAY - LEONXL - AL */
 
 if(mysqli_num_rows($result) > 0){
 
 while($res = mysqli_fetch_assoc($result)){
 $commentcheck = $res['Comment']; //IT MUST HAVE A COMMENT TO DISPLAY ANYTHING
-$staramount = $res['Rating']; //RETURNS THE AMOUNT OF STARS - LEONXL - AL 
-$ReviewNickname = $res['ReviewNickname']; 
+$staramount = $res['Rating']; //RETURNS THE AMOUNT OF STARS - LEONXL - AL
+$username = $res[''];
 
+ for($x = 0; $x<$staramount;$x++){ //LOGIC: IF $x (is less than) $staramount(example:3) Result: Print 3 stars - LEONXL - AL
+  echo "<span class='fa fa-star checked'></span>"; //ECHO THE STAR IMAGE - LEONXL - AL
+  }
 
- for($x = 0; $x<$staramount;$x++){ //LOGIC: IF $x (is less than) $staramount(example:3) Result: Print 3 stars - LEONXL - AL  
-  echo "<span class='fa fa-star checked'></span>"; //ECHO THE STAR IMAGE - LEONXL - AL 
-  } 
-   
   $userId = $res['UserId'];
-  if($userId == NULL && $commentcheck && $ReviewNickname == 'None'){
+  if($userId == NULL && $commentcheck){
       echo "<br>" . '<strong>Anonymous</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>";
-      echo "<hr class='rating-border'>";  
+      echo "<hr class='rating-border'>";
     }
-   elseif($userId == $id && $commentcheck && $ReviewNickname == 'None'){
-    echo "<br>" .'<strong>' . $name . '</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>"; //SHOWS COMMENTS - LEONXL - AL 
+   elseif($userId == $id && $commentcheck){
+    echo "<br>" .'<strong>' . $name . '</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>"; //SHOWS COMMENTS - LEONXL - AL
     echo "<hr class='rating-border'>";
-  } elseif($userId == $id && $commentcheck && $ReviewNickname == $nickname){
-    echo "<br>" .'<strong>' . $ReviewNickname . '</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>"; //SHOWS COMMENTS - LEONXL - AL 
-    echo "<hr class='rating-border'>";
-  } elseif($userId != $id && $commentcheck && $ReviewNickname != 'None'){
-    echo "<br>" .'<strong>' . $ReviewNickname . '</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>"; //SHOWS COMMENTS - LEONXL - AL
-    echo "<hr class='rating-border'>";
-  }  elseif($userId != NULL && $commentcheck && $ReviewNickname == 'None'){
+  } else{
     echo "<br>" .'<strong>' . $ForUserName . '</strong>' . ' ' . wordwrap($res['Comment'],50,"<br>\n",True) . "<br>"; //SHOWS COMMENTS - LEONXL - AL
     echo "<hr class='rating-border'>";
   }
-  
 
-} 
+
+}
 
 }
 echo "</div>";
-?>  
+?>
 
 
 </p>
@@ -305,6 +289,120 @@ echo "</div>";
       </div>
 
     </footer>
+    <!-- CSS-->
+    <style>
+      a.lightbox img {
+      border: 3px solid white;
+      box-shadow: 0px 0px 8px rgba(0,0,0,.3);
+      }
+
+       Styles the lightbox, removes it from sight and adds the fade-in transition */
+
+      .lightbox-target {
+      position: fixed;
+      top: -100%;
+      width: 100%;
+      background: rgba(0,0,0,.7);
+      width: 100%;
+      opacity: 0;
+      -webkit-transition: opacity .5s ease-in-out;
+      -moz-transition: opacity .5s ease-in-out;
+      -o-transition: opacity .5s ease-in-out;
+      transition: opacity .5s ease-in-out;
+      overflow: hidden;
+      }
+
+      /* Styles the lightbox image, centers it vertically and horizontally, adds the zoom-in transition and makes it responsive using a combination of margin and absolute positioning */
+
+      .lightbox-target img {
+      margin: auto;
+      position: absolute;
+      top: 0;
+      left:0;
+      right:0;
+      bottom: 0;
+      max-height: 0%;
+      max-width: 0%;
+      border: 3px solid white;
+      box-shadow: 0px 0px 8px rgba(0,0,0,.3);
+      box-sizing: border-box;
+      -webkit-transition: .5s ease-in-out;
+      -moz-transition: .5s ease-in-out;
+      -o-transition: .5s ease-in-out;
+      transition: .5s ease-in-out;
+      }
+
+      /* Styles the close link, adds the slide down transition */
+
+      a.lightbox-close {
+      display: block;
+      width:50px;
+      height:50px;
+      box-sizing: border-box;
+      background: white;
+      color: black;
+      text-decoration: none;
+      position: absolute;
+      top: -80px;
+      right: 0;
+      -webkit-transition: .5s ease-in-out;
+      -moz-transition: .5s ease-in-out;
+      -o-transition: .5s ease-in-out;
+      transition: .5s ease-in-out;
+      }
+
+      /* Provides part of the "X" to eliminate an image from the close link */
+
+      a.lightbox-close:before {
+      content: "";
+      display: block;
+      height: 30px;
+      width: 1px;
+      background: black;
+      position: absolute;
+      left: 26px;
+      top:10px;
+      -webkit-transform:rotate(45deg);
+      -moz-transform:rotate(45deg);
+      -o-transform:rotate(45deg);
+      transform:rotate(45deg);
+      }
+
+      /* Provides part of the "X" to eliminate an image from the close link */
+
+      a.lightbox-close:after {
+      content: "";
+      display: block;
+      height: 30px;
+      width: 1px;
+      background: black;
+      position: absolute;
+      left: 26px;
+      top:10px;
+      -webkit-transform:rotate(-45deg);
+      -moz-transform:rotate(-45deg);
+      -o-transform:rotate(-45deg);
+      transform:rotate(-45deg);
+      }
+
+      /* Uses the :target pseudo-class to perform the animations upon clicking the .lightbox-target anchor */
+
+      .lightbox-target:target {
+      opacity: 1;
+      top: 0;
+      bottom: 0;
+      }
+
+      .lightbox-target:target img {
+      max-height: 100%;
+      max-width: 100%;
+      }
+
+      .lightbox-target:target a.lightbox-close {
+      top: 0px;
+      }
+    </style>
+
 
 
 </body>
